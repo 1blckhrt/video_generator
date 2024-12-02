5bf866efdd870f613ccc3582dc066f8f345fea1e
1blckhrt <williams.1691@wright.edu>
2024-12-01 22:11:02 -0500

    feat: basic gui header

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("hello world")
+from tkinter import Label, Tk
+
+
+def main():
+    root = Tk()
+    window = Label(root, text="(@1blckhrt) Audio and Image to Video Converter")
+    window.pack()
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
