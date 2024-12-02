c14ef42e19d09a539d5615bfb660338fb4fd78c6
1blckhrt <williams.1691@wright.edu>
2024-12-01 22:42:08 -0500

    feat: add get audio and video file button

--- src/main.py
+++ src/main.py
@@ -31,9 +31,27 @@ def main():
         fg="white",
     )
 
+    get_audio_button = tk.Button(
+        window,
+        text="Open Audio File",
+        font=("Arial", 13),
+        bg="blue",
+        fg="white",
+    )
+
+    get_image_button = tk.Button(
+        window,
+        text="Open Image File",
+        font=("Arial", 13),
+        bg="blue",
+        fg="white",
+    )
+
     title_label.pack(padx=20, pady=20)
     audio_file_explorer_label.pack()
+    get_audio_button.pack()
     image_file_explorer_label.pack()
+    get_image_button.pack()
 
     window.mainloop()
 
