9c3f3ff3741f67c4a83b6747ccc955edf1928f05
1blckhrt <williams.1691@wright.edu>
2024-12-01 22:24:42 -0500

    feat: add color and title label

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
-from tkinter import Tk
+import tkinter as tk
 
 
 def main():
-    window = Tk()
+    window = tk.Tk()
     window.title("(@1blckhrt) Audio and Image Merger")
+    window.geometry("500x500")
+    window.config(background="black", padx=20, pady=20)
+
+    title_label = tk.Label(
+        window,
+        text="Audio and Image Merger",
+        font=("Arial", 24),
+        bg="black",
+        fg="white",
+    )
+
+    title_label.pack(padx=20, pady=20)
+
     window.mainloop()
 
 
