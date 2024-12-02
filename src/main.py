7a63c713bcaa5d5997e79bc882019e72a7554cdc
1blckhrt <williams.1691@wright.edu>
2024-12-01 22:31:10 -0500

    feat: add labels for image and audio

--- src/main.py
+++ src/main.py
@@ -15,7 +15,25 @@ def main():
         fg="white",
     )
 
+    audio_file_explorer_label = tk.Label(
+        window,
+        text="Select Audio File",
+        font=("Arial", 16),
+        bg="black",
+        fg="white",
+    )
+
+    image_file_explorer_label = tk.Label(
+        window,
+        text="Select Image File",
+        font=("Arial", 16),
+        bg="black",
+        fg="white",
+    )
+
     title_label.pack(padx=20, pady=20)
+    audio_file_explorer_label.pack()
+    image_file_explorer_label.pack()
 
     window.mainloop()
 
